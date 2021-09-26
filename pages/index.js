7df17d6f7d48a5346345794dfbc5c8dccdf9ef77
Jessica Leverett <Jessak1202@gmail.com>
2021-09-26 15:36:46 -0500

    her

--- pages/index.js
+++ pages/index.js
@@ -47,25 +47,25 @@ export default function Home() {
             </p>
           </a>
         </div>
+        <h1> Contact </h1>
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" />
+          </p>
+          <p>
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" name="email" />
+          </p>
+          <p>
+            <label htmlFor="message">Message</label>
+            <textarea id="message" name="message"></textarea>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </main>
-      <h1> Contact </h1>
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" />
-        </p>
-        <p>
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" />
-        </p>
-        <p>
-          <label htmlFor="message">Message</label>
-          <textarea id="message" name="message"></textarea>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
 
       <footer className={styles.footer}>
         <a
